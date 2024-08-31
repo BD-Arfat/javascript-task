c0795c0281f3e402719b30027f3188b5dcd56755
hasanulhasan <hasanulkarimh@gmail.com>
2024-08-31 23:28:52 +0600

    complite task-2.js file

--- task-2.js
+++ task-2.js
@@ -24,3 +24,51 @@ if (bmi <= 18.5) {
 } else {
   //console.log(" Otherwise, you are obese.");
 }
+
+// task 3
+var number = 90;
+
+if (number <= 59) {
+  //console.log("f");
+} else if (number <= 69) {
+  //console.log("d");
+} else if (number <= 79) {
+  //console.log("c");
+} else if (number <= 89) {
+  //console.log("b");
+} else if (number <= 100) {
+  //console.log("a");
+} else {
+  //console.log("Amar mone hoy apnar konu ek jaigai vul hocche");
+}
+
+// task 5
+var num1 = 283;
+var num2 = 30;
+if (num1 >= num2) {
+  var totalNumber = num1 * 2;
+  //console.log(totalNumber);
+} else {
+  var sum = num1 + num2;
+  //console.log(sum);
+}
+
+// task 6
+
+var age = 90;
+var taka = 500;
+
+if (age <= 10) {
+  //console.log("free");
+} else if (age === "student") {
+  var discount = (taka * 50) / 100;
+  var totalTaka = taka - discount;
+  //console.log(totalTaka);
+} else if (age >= 60) {
+  var discount = (taka * 15) / 100;
+  var totalTaka = taka - discount;
+  //console.log(totalTaka);
+} else {
+  var totalTaka = taka + 800;
+  //console.log(totalTaka);
+}
