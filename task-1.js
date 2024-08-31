8c8a875d8cf8919a9d5a7600802110dceb46504d
hasanulhasan <hasanulkarimh@gmail.com>
2024-08-31 22:51:45 +0600

    first commit

---
+++ task-1.js
@@ -0,0 +1,26 @@
+// task 1
+var orange = 300;
+var apple = 120;
+var taka = 1000;
+
+var totalPrice = orange + apple;
+var returntaka = taka - totalPrice;
+//console.log(returntaka);
+
+// task 2
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.5;
+
+var totalMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+var averageMarks = totalMarks / 5;
+//console.log(parseFloat(averageMarks.toFixed(2)));
+
+// task 3
+
+var num = 119;
+var remainder = num % 5;
+//console.log(remainder);
---
+++ task-2.js
@@ -0,0 +1,26 @@
+var shops = 400;
+var free = "coke";
+
+if (shops >= 500) {
+  //console.log(
+  // `apni jehetu ${shops} takar jinish kinechen, tai amra apnake ekta ${free} ditechi`
+  // );
+} else {
+  //console.log(
+  //`apne jehetu 500 takar niche jinish kinechen, tai apnake ekta 30 taka diye coke kinte hobe....`
+  //);
+}
+
+// task 2
+
+var bmi = 10;
+
+if (bmi <= 18.5) {
+  //console.log("you are underweight.");
+} else if (bmi <= 24.9) {
+  //console.log("you are normal.");
+} else if (bmi <= 29.5) {
+  //console.log("you are overweight.");
+} else {
+  //console.log(" Otherwise, you are obese.");
+}
