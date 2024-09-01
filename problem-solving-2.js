4aa7a9e29e79399da52627f6a43e3355ceab1a0d
hasanulhasan <hasanulkarimh@gmail.com>
2024-09-01 19:34:47 +0600

    complite taks and problem solving

---
+++ problem-solving-2.js
@@ -0,0 +1,24 @@
+var salary = 30000;
+if (salary >= 100000) {
+  var bonasSalary = (salary * 10) / 100;
+  var totalSalary = salary + bonasSalary;
+ // console.log(
+   // `jehetu apnar salary ${salary}$ taka tai discount shoho apni amader theke paben ${totalSalary}$ taka......`
+ // );
+} else if (salary <= 50000 && salary >= 10000) {
+  var bonasSalary = (salary * 7) / 100;
+  var totalSalary = salary + bonasSalary;
+  //console.log(
+   // `jehetu apnar salary ${salary}$ taka tai discount shoho apni amader theke paben ${totalSalary}$ taka......`
+  //);
+} else {
+  var bonasSalary = (salary * 5) / 100;
+  var totalSalary = salary + bonasSalary;
+  //console.log(
+    //`jehetu apnar salary ${salary}$ taka tai discount shoho apni amader theke paben ${totalSalary}$ taka......`
+  //);
+}
+
+// task number 2
+
+
